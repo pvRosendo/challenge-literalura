f4ed7a75f9126b067996929ab1944b7723f8797f
Paulo Vitor Rosendo <paulo.vitor.rsd@poli.ufrj.br>
2024-05-15 13:42:57 -0300

    init the project

---
+++ src/main/java/com/rosendo/challengeliteralura/ChallengeLiteraluraApplication.java
@@ -0,0 +1,13 @@
+package com.rosendo.challengeliteralura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiteraluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiteraluraApplication.class, args);
+	}
+
+}
